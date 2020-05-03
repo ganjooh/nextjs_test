264034a052eaa1920e6a70fda18672ce08e01510
ganjoo <hiteshganjoo@gmail.com>
2020-05-03 22:10:36 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import fetch from 'isomorphic-unfetch'
+import Head from 'next/head'
+import Plyr from 'react-plyr';
 
 export default class extends Component {
   static async getInitialProps() {
@@ -14,7 +16,16 @@ export default class extends Component {
 
   render () {
     return (
+
       <div>
+      <Head>
+      <title>Test</title>
+      <link href="/static/master.css" rel="stylesheet" key="test"/>
+    </Head>
+    <Plyr
+      type="youtube"
+      videoId="CDFN1VatiJA"
+    />
         Wohoooo
         <div>
           {this.props.title}
